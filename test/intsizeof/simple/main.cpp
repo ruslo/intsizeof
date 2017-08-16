01223736c6fb91ffb0306425cc0ed01b0df710c3
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-08-16 15:07:13 +0300

    Fix test

--- test/intsizeof/simple/main.cpp
+++ test/intsizeof/simple/main.cpp
@@ -1,34 +1,34 @@
 // Copyright (c) 2014, Ruslan Baratov
 // All rights reserved.
 
-#include <int_sizeof.h> // INT_SIZEOF_PTRDIFF
+#include <intsizeof.h> // INT_SIZEOF_PTRDIFF
 
 #include <cstddef> // ptrdiff_t
 #include <signal.h> // sig_atomic_t
 #include <cwchar> // wint_t
 
 int main() {
-  static_assert(INT_SIZEOF_PTRDIFF == sizeof(ptrdiff_t), "");
-  static_assert(INT_SIZEOF_SIZE == sizeof(size_t), "");
-  static_assert(INT_SIZEOF_SIG_ATOMIC == sizeof(sig_atomic_t), "");
-  static_assert(INT_SIZEOF_WCHAR == sizeof(wchar_t), "");
-  static_assert(INT_SIZEOF_WINT == sizeof(wint_t), "");
-  static_assert(INT_SIZEOF_CHAR == sizeof(char), "");
+  static_assert(INTSIZEOF_PTRDIFF == sizeof(ptrdiff_t), "");
+  static_assert(INTSIZEOF_SIZE == sizeof(size_t), "");
+  static_assert(INTSIZEOF_SIG_ATOMIC == sizeof(sig_atomic_t), "");
+  static_assert(INTSIZEOF_WCHAR == sizeof(wchar_t), "");
+  static_assert(INTSIZEOF_WINT == sizeof(wint_t), "");
+  static_assert(INTSIZEOF_CHAR == sizeof(char), "");
 
-  static_assert(INT_SIZEOF_SCHAR == sizeof(signed char), "");
-  static_assert(INT_SIZEOF_SHRT == sizeof(signed short), "");
-  static_assert(INT_SIZEOF_INT == sizeof(int), "");
-  static_assert(INT_SIZEOF_LONG == sizeof(long), "");
-  static_assert(INT_SIZEOF_LLONG == sizeof(long long), "");
+  static_assert(INTSIZEOF_SCHAR == sizeof(signed char), "");
+  static_assert(INTSIZEOF_SHRT == sizeof(signed short), "");
+  static_assert(INTSIZEOF_INT == sizeof(int), "");
+  static_assert(INTSIZEOF_LONG == sizeof(long), "");
+  static_assert(INTSIZEOF_LLONG == sizeof(long long), "");
 
-  static_assert(INT_SIZEOF_UCHAR == sizeof(unsigned char), "");
-  static_assert(INT_SIZEOF_USHRT == sizeof(unsigned short), "");
-  static_assert(INT_SIZEOF_UINT == sizeof(unsigned int), "");
-  static_assert(INT_SIZEOF_ULONG == sizeof(unsigned long), "");
-  static_assert(INT_SIZEOF_ULLONG == sizeof(unsigned long long), "");
+  static_assert(INTSIZEOF_UCHAR == sizeof(unsigned char), "");
+  static_assert(INTSIZEOF_USHRT == sizeof(unsigned short), "");
+  static_assert(INTSIZEOF_UINT == sizeof(unsigned int), "");
+  static_assert(INTSIZEOF_ULONG == sizeof(unsigned long), "");
+  static_assert(INTSIZEOF_ULLONG == sizeof(unsigned long long), "");
 
   // sanity check
-  static_assert(INT_SIZEOF_CHAR == 1, "");
-  static_assert(INT_SIZEOF_UCHAR == 1, "");
-  static_assert(INT_SIZEOF_SCHAR == 1, "");
+  static_assert(INTSIZEOF_CHAR == 1, "");
+  static_assert(INTSIZEOF_UCHAR == 1, "");
+  static_assert(INTSIZEOF_SCHAR == 1, "");
 }
